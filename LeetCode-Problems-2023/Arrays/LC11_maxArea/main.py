5d9c9b6b1cd719becbd4ebc85ef8bbc6c48d1e45
Suman Debnath <56056673+debnsuma@users.noreply.github.com>
2023-02-18 19:33:41 -0500

    array problems

--- LeetCode-Problems-2023/Arrays/LC11_maxArea/main.py
+++ LeetCode-Problems-2023/Arrays/LC11_maxArea/main.py
@@ -1,2 +1,20 @@
 class Solution:
     def maxArea(self, height):
+        
+        start = 0
+        end = len(height) - 1
+        current_area = 0 
+        max_area = 0
+        while start < end:
+            current_area = (end - start) * min(height[start], height[end])
+            max_area = max(current_area, max_area)
+            # print(start, end, current_area, max_area, height[start], height[end])
+
+            if height[start] < height[end]:
+                start += 1
+            else:
+                end -= 1
+            
+        return max_area
+
+
--- LeetCode-Problems-2023/Arrays/LC1_TwoSum/main.py
+++ LeetCode-Problems-2023/Arrays/LC1_TwoSum/main.py
@@ -1,3 +1,12 @@
 class Solution:
     def twoSum(self, nums, target):
-        pass 
+        nums_seen = dict()
+        
+        for i in range(len(nums)):
+            present_element = nums[i]
+            if target - present_element in nums_seen:
+                return nums_seen[target - present_element], i 
+            elif present_element not in nums_seen:
+                nums_seen[present_element] = i 
+                    
+        return False
