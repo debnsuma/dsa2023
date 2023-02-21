fe47a100631b8e176b71acb26a3dc748f75cf6e0
Suman Debnath <56056673+debnsuma@users.noreply.github.com>
2023-02-21 08:22:53 -0500

    LL problems

--- LeetCode-Problems-2023/Linked_List/LC206_reverseList/main.py
+++ LeetCode-Problems-2023/Linked_List/LC206_reverseList/main.py
@@ -1,8 +1,22 @@
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution:
+    
     def reverseList(self, head):
+        
+        previous = None 
+        current = head
+        
+        
+        while current is not None:
+            next = current.next 
+            current.next = previous
+            previous, current = current, next
+            
+            print(previous.val)
+
+        return previous 
