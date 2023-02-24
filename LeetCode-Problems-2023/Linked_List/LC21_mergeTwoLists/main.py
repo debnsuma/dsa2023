966daae0be9da816649b3e427d8d88ae6c8aecd7
Suman Debnath <56056673+debnsuma@users.noreply.github.com>
2023-02-24 06:29:31 -0500

    added LL problems

--- LeetCode-Problems-2023/Linked_List/LC21_mergeTwoLists/main.py
+++ LeetCode-Problems-2023/Linked_List/LC21_mergeTwoLists/main.py
@@ -1,8 +1,24 @@
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def mergeTwoLists(self, l1, l2):
+    def mergeTwoLists(self, head_1, head_2):
+        
+        dummy_head = ListNode("Filler")
+        tail = dummy_head
+        
+        while head_1 and head_2:
+            
+            if head_1.val < head_2.val:
+                tail.next = head_1
+                head_1 = head_1.next 
+            else:
+                tail.next = head_2
+                head_2 = head_2.next 
+                
+            tail = tail.next 
+        
+        tail.next = head_1 or head_2
+        
+        return dummy_head.next 
